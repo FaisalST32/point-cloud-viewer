5a590124bdae01999772e28aef85ec3c074952c9
Faisal Rashid <faisalrashid32@outlook.com>
2021-10-23 21:01:43 +0530

    navigatable point clouds

--- src/PCDLoader.ts
+++ src/PCDLoader.ts
@@ -2,11 +2,16 @@ import * as THREE from 'three';
 import { PCDLoader as ThreePCDLoader } from 'three/examples/jsm/loaders/PCDLoader';
 
 export class PCDLoader {
-	scene: THREE.Scene;
-	loader: ThreePCDLoader;
+	private scene: THREE.Scene;
+	private loader: ThreePCDLoader;
+	private pointClouds: THREE.Points[];
+	private currentPC?: THREE.Points;
+	private mode: 'aggregate' | 'single' = 'single';
+
 	constructor(scene: THREE.Scene) {
 		this.scene = scene;
 		this.loader = new ThreePCDLoader();
+		this.addEventListeners();
 	}
 
 	async loadAsync(url: string): Promise<THREE.Points> {
@@ -18,7 +23,7 @@ export class PCDLoader {
 		});
 	}
 
-	async loadManyAsync(urls: string[]): Promise<THREE.Points[]> {
+	async loadManyAsync(urls: string[]): Promise<number> {
 		const promises = urls.map(
 			(url) =>
 				new Promise<THREE.Points>((res, rej) => {
@@ -28,11 +33,80 @@ export class PCDLoader {
 				})
 		);
 
-		const points: THREE.Points[] = await Promise.all(promises);
-		points.forEach((p) => {
-			this.scene.add(p);
-		});
+		this.pointClouds = await Promise.all(promises);
+		this.renderPC(this.pointClouds[0]);
+		return this.pointClouds.length;
+	}
 
-		return points;
+	prevPC() {
+		if (!this.pointClouds?.length || this.mode === 'aggregate') {
+			return;
+		}
+
+		const currentPCIndex = this.pointClouds.indexOf(this.currentPC!);
+		const isFirstPC = currentPCIndex === 0;
+		const prevIndex = isFirstPC
+			? this.pointClouds.length - 1
+			: currentPCIndex - 1;
+		this.renderPC(this.pointClouds[prevIndex]);
+	}
+
+	nextPC() {
+		if (!this.pointClouds?.length || this.mode === 'aggregate') {
+			return;
+		}
+
+		const currentPCIndex = this.pointClouds.indexOf(this.currentPC!);
+		const isFinalPC = currentPCIndex === this.pointClouds.length - 1;
+		const nextIndex = isFinalPC ? 0 : currentPCIndex + 1;
+		this.renderPC(this.pointClouds[nextIndex]);
+	}
+
+	private async renderPC(points: THREE.Points) {
+		if (this.currentPC) {
+			this.scene.remove(this.currentPC);
+		}
+		this.currentPC = points;
+		this.scene.add(this.currentPC);
+	}
+
+	private addEventListeners() {
+		document.addEventListener(
+			'keyup',
+			this.addNavigationKeysListeners.bind(this)
+		);
+	}
+
+	private aggregatePoints() {
+		if (this.currentPC) {
+			this.scene.remove(this.currentPC);
+			this.currentPC = undefined;
+		}
+
+		this.pointClouds.forEach((pc) => this.scene.add(pc));
+	}
+
+	private splitFrames() {
+		this.pointClouds.forEach((pc) => this.scene.remove(pc));
+		this.renderPC(this.pointClouds[0]);
+	}
+
+	private addNavigationKeysListeners(e: KeyboardEvent) {
+		switch (e.key.toLowerCase()) {
+			case 'arrowleft':
+				this.prevPC();
+				break;
+			case 'arrowright':
+				this.nextPC();
+				break;
+			case 'a':
+				this.aggregatePoints();
+				break;
+			case 'escape':
+				this.splitFrames();
+				break;
+			default:
+				break;
+		}
 	}
 }
